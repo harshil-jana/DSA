5f32a893a6d59da7da273453ff554ac79a522c94
harshil-jana <harshil.jana24@gmail.com>
2026-07-16 23:14:27 +0530

    Completed Insertion Sort

---
+++ insertion_sort.py
@@ -0,0 +1,20 @@
+def insertion_sort(arr):
+    n = len(arr)
+
+    for i in range(1,n):
+        current = 
+
+
+
+
+
+
+
+
+
+
+
+
+unsorted = [12,25,11,34,90,22]
+sorted = insertion_sort(unsorted)
+print(f"Sorted list = {sorted}")
