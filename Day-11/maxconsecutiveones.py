6ae5ad7d96ea88a0f5ae0e5dd1fe9e0cac569ee1
harshil-jana <harshil.jana24@gmail.com>
2026-07-26 22:50:34 +0530

    Completed 'Maximum Consecutive Ones'

---
+++ Day-11/maxconsecutiveones.py
@@ -0,0 +1,14 @@
+def max_consecutive(arr):
+    count = 0
+    max_count = 0
+
+    for i in range(0,len(arr)):
+        if arr[i] == 1:
+            count += 1
+        else:
+            max_count = max(max_count, count)
+            count = 0
+    return max_count
+
+nums = [1,0,1,1,0,1]
+print(max_consecutive(nums))
