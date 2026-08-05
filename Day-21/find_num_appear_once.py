a32462e2af7a748caf1bc50b35259c4acd65362c
harshil-jana <harshil.jana24@gmail.com>
2026-08-05 23:04:49 +0530

    Completed Finding Num Which Appear Only Once Using Linear Search

---
+++ Day-21/find_num_appear_once.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findNumappearOne(self, arr: list[int]) -> int:
+        n = len(arr)
+
+        for i in range(0,n):
+            num = arr[i]
+            count = 0
+
+            for j in range(0,n):     # Check how many times number appeared
+                if arr[j] == num:
+                    count += 1
+
+            if count == 1:  # If it appears once, return the number
+                return num
+
+        return -1
+    
+sol = Solution()
+arr = [2,2,1]
+print(sol.findNumappearOne(arr))          
+        
+        
