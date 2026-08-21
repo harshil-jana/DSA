29ca5b995877c252e2c46091cba0b6a203179e59
harshil-jana <harshil.jana24@gmail.com>
2026-08-21 23:41:50 +0530

    Updated Max Sum Subarray Using Sliding Window

--- Day-35/max_sum_subarr_SW.py
+++ Day-35/max_sum_subarr_SW.py
@@ -1,11 +1,11 @@
 class Solution:
-    def maxSubarraySum(self, arr, k):
+    def maxSubarraySum(self, nums, k):
         total = 0
         max_sum = 0
-        n = len(arr)
+        n = len(nums)
 
         for i in range(0,k):
-            total += arr[i]
+            total += nums[i]
             max_sum = max(max_sum, total)
 
         if k == n:
@@ -15,7 +15,7 @@ class Solution:
         j = k
 
         while j < n:
-            total = total + arr[j] - arr[i-1]
+            total = total + nums[j] - nums[i-1]
             max_sum = max(max_sum, total)
             i += 1
             j += 1
@@ -24,4 +24,4 @@ class Solution:
 
 nums = [8819, 674, 8816, 7705, 5699, 5383, 6177, 2113, 1992]
 k = 1 
-print(Solution().maxSubarraySum(nums, k))
+print(Solution().maxSubnumsaySum(nums, k))
